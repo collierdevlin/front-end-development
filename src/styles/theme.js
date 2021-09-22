ec870285d04b5669026768d7cd4c9323ce56f893
Collier <jocodev@gmail.com>
2021-09-22 07:30:57 -0400

    Adjust theme for buttons

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,10 +2,17 @@ import { createTheme } from 'baseui'
 
 const primitives = {
   primaryFontFamily: 'Work Sans',
+  accent: '#ffc000',
+  accent100: '#ffda00',
+  accent200: '#646877'
 }
 
 const overrides = {
-  //
+  colors: {
+    buttonPrimaryFill: primitives.accent,
+    buttonPrimaryHover: primitives.accent100,
+    buttonDisabledFill: primitives.accent200
+  }
 }
 
 const theme = createTheme(primitives, overrides)
